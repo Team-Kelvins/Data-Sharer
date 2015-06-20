9f6f1bd81d80502cfe9359beda9fd681f0f62b70
Kelvin Yip <autilite@gmail.com>
2015-06-20 12:13:16 -0700

    Create signup form using php

--- signup.php
+++ signup.php
@@ -9,24 +9,52 @@
 		<h1 class="specialHeader"> Sign up </h1>
 		<div>
 			<form method="POST" action="signup.php" class="signUpForm">
-				<p> Last Name </p>
-				<input type="text" placeholder="Smith" class="loginInput" name="lName" />
-				<p> First Name </p>
-				<input type="text" placeholder="John" class="loginInput" name="fName" />
-				<p> Username </p>
-				<input type="text" placeholder="John000" class="loginInput" name="uName" />
-				<p> Email </p>
-				<input type="text" placeholder="John@example.com" class="loginInput" name="uEmail" />
-				<p> Password </p>
-				<input type="password" class="loginInput" name="uPassword" id="password1" />
-				<p> Confirm Password </p>
-				<input type="password" class="loginInput" name="confirmPassword" id="passwordRepeat" />
-				<input type="submit" name="submitBtn" value="Sign up" />
+                <?php
+                    createFormElement("text", "Last Name", "Smith", "loginInput", "lName", null);
+                    createFormElement("text", "First name", "John", "loginInput", "fName", null);
+                    createFormElement("text", "Username", "John000", "loginInput", "uName", null);
+                    createFormElement("text", "Email", "John@example.com", "loginInput", "uEmail", null);
+                    createFormElement("password", "Password", null, "loginInput", "uPassword", "password1");
+                    createFormElement("password", "Confirm Password", null, "loginInput", "confirmPassword", null);
+                    createFormSubmit("submitBtn", "Sign up");
+                ?>
 			</form>
 		</div>
 		<p> By signing up, you agree to Data Sharer's <a href="#" class="terms"> Terms of Service </a> </p>
 	</body>
 </html>
+
+<?php
+    function createFormElement($type="text", $title, $placeholder, $class, $name, $id) {
+        echo "<div class=\"form-element\">";
+        if ($title != null) {
+            echo $title;
+        }
+        echo "<input ";
+        echo "type=\"$type\" ";
+        if ($placeholder != null) {
+            echo "placeholder=\"$placeholder\" ";
+        }
+        if ($class != null) {
+            echo "class=\"$class\" ";
+        }
+        if ($name != null) {
+            echo "name=\"$name\" ";
+        }
+        if ($id != null) {
+            echo "id=\"$id\" ";
+        }
+        echo "/>";
+        echo "</div>";
+    }
+
+    function createFormSubmit($name, $text) {
+        echo "<div class=\"form-element\" \"submit-button\">";
+        // TODO add class
+        echo "<input type=\"submit\" name=\"$name\" value=\"$text\"/>";
+        echo "</div>";
+    }
+?>
 
 <?php 
 
